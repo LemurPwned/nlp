7fdac98d1e63836dcc86c5c46c6aebd441b85f70
LemurPwned <jakub.mojsiejuk@gmail.com>
2019-05-28 16:21:48 +0200

    Markov

--- notes.py
+++ notes.py
@@ -21,7 +21,6 @@ class Notes:
         self.word_index = {}
         self.stop_words = open('./lab5/stopwords.txt', 'r').read().split('\n')
         print(f"{Fore.YELLOW}Loading saved root...{Fore.RESET}")
-        self.root_dict = json.load(open('./root_dict.json', 'r'))
         # if os.path.isfile('./lab5/notes_idf.json'):
         #     print(f"{Fore.GREEN}Loading saved statistics...{Fore.RESET}")
         #     self.idf = json.load(open('./lab5/notes_idf.json', 'r'))
@@ -53,34 +52,16 @@ class Notes:
         json.dump(self.tf,  open('./lab5/notes_tf.json', 'w'))
         pkl.dump(self.notes, open('./lab5/notes2.pkl', 'wb'))
 
-    def graph_representation(self, window_size=3):
-        test_size = 50
-        filename = f'./lab5/graph_{window_size}_{test_size}.pkl'
+    def graph(self, test_set, window_size=2):
+        filename = f'./lab5/graph_{window_size}_{len(test_set)}.pkl'
         print(f"{Fore.GREEN}Calculating graphs...{Fore.RESET}")
         document_vectors = []
         # create word index
-        if not os.path.isfile('test_note_dmp.pkl'):
-            test_set = np.random.choice(list(self.notes.values()), (100, ))
-            pkl.dump(test_set, open('test_note_dmp.pkl', 'wb'))
-            print(test_set[0])
-        else:
-            test_set = pkl.load(open('test_note_dmp.pkl', 'rb'))
-        if not os.path.isfile():
+        if not os.path.isfile(filename):
+            print(f'Words in index: {len(self.word_index)}')
             for note in test_set:
-                for w in note:
-                    if w not in self.stop_words:
-                        try:
-                            wp = self.root_dict[w]
-                        except KeyError:
-                            wp = w
-                            self.root_dict[wp] = wp
-                        if wp not in self.word_index.keys():
-                            self.word_index[wp] = len(self.word_index)
-            print(len(self.word_index))
-            for note in test_set:
-                # G = nx.DiGraph()
                 ilen = len(self.word_index)
-                vsm = np.zeros(shape=(ilen**2,), dtype=int)
+                vsm = np.zeros(shape=(ilen**2,), dtype=np.uint8)
                 nlen = len(note)
                 for i, w in enumerate(note[1:]):  # first is ID
                     if w in self.stop_words:
@@ -90,73 +71,80 @@ class Notes:
                             break
                         if note[j] in self.stop_words:
                             continue
-                        vsm[self.word_index[self.root_dict[note[j]]] *
-                            ilen + self.word_index[self.root_dict[w]]] += 1
-                        # weight = 1.0
-                        # if edge in G:
-                        #     weight += G[note[j]][w]['weight']
-                        #     print(weight)
-                        # G.add_edge(*edge, weight=weight)
+                        vsm[self.word_index[note[j]] *
+                            ilen + self.word_index[w]] += 1
                 document_vectors.append(vsm)
-            pkl.dump(document_vectors, open(filename, 'rb'))
+            pkl.dump(document_vectors, open(filename, 'wb'))
         else:
             print(f'Loading saved graphs...')
             document_vectors = pkl.load(
-                open(filename, 'wb'))
-        del self.root_dict
-        # clusterize
-        print(len(document_vectors))
-        classes = 10
-        pca = TruncatedSVD(n_components=5)
-        features_t = pca.fit(np.array(document_vectors))
-        # del document_vectors
-        print(f"{Fore.MAGENTA}Projecting with TSNE...{Fore.RESET}")
-        tsne = TSNE(n_components=2, perplexity=5)
-        Y = tsne.fit_transform(features_t)
+                open(filename, 'rb'))
+        return np.array(document_vectors)
 
-        color_list = [['#'+''.join([random.choice('0123456789ABCDEF')
-                                    for j in range(6)])] for i in range(classes)]
-        mb = MiniBatchKMeans(n_clusters=classes, batch_size=100)
-        # Y = np.array(document_vectors)
-        mb.fit_transform(Y)
-        labels = mb.labels_
-        for i, label in enumerate(np.unique(labels)):
-            plt.scatter(Y[labels == label, 0],
-                        Y[labels == label, 1], c=color_list[i], label=str(i))
-        plt.title("Klasyfikacja PAP")
-        plt.legend()
-        plt.show()
+    def classify_and_evaluate(self, n_classes=5, test_size=50):
+        dmp_filename = f'./lab5/test_{test_size}_dmp_note.pkl'
+        if not os.path.isfile(dmp_filename):
+            print(f'Reducing to stems...')
+            test_set = [self.notes[str(i+1).zfill(6)]
+                        for i in range(test_size)]
+            self.root_dict = json.load(open('./root_dict.json', 'r'))
+            new_test_set = []
+            for note in test_set:
+                new_note = []
+                for w in note:
+                    if w not in self.stop_words:
+                        try:
+                            wp = self.root_dict[w]
+                        except KeyError:
+                            wp = w
+                            self.root_dict[wp] = wp
+                        if wp not in self.word_index.keys():
+                            self.word_index[wp] = len(self.word_index)
+                        new_note.append(wp)
+                new_test_set.append(new_note)
+            test_set = new_test_set
+            del self.root_dict
+            pkl.dump(test_set, open(dmp_filename, 'wb'))
+            pkl.dump(self.word_index, open('./lab5/word_index.pkl', 'wb'))
+        else:
+            test_set = pkl.load(open(dmp_filename, 'rb'))
+            self.word_index = pkl.load(open('./lab5/word_index.pkl', 'rb'))
+        print(
+            f'{Fore.BLUE}Read test set of size {len(test_set)}, {len(test_set[0])}, {len(test_set[1])}{Fore.RESET}')
+        for i, method in enumerate([self.tfidf, self.graph, self.graph, self.graph, self.graph]):
+            if i == 0:
+                documents = method(test_set)
+            else:
+                documents = method(test_set, i+1)
+            pca = TruncatedSVD(n_components=10)
+            print(f"\t{Fore.MAGENTA}Reducing with SVD...{Fore.RESET}")
+            print(documents.shape)
+            features_reduced = pca.fit_transform(documents)
+            train_notes = features_reduced[:-1, :]
+            test_note = features_reduced[-1,
+                                         :].reshape(1, -1)  # last is test one
+            mb = MiniBatchKMeans(n_clusters=n_classes, batch_size=100)
+            mb.fit(train_notes)
+            test_label = mb.predict(test_note)
+            train_labels = mb.labels_
+            similar_notes_indices = np.nonzero(
+                train_labels == test_label)[0].tolist()
+            similar_notes_indices = [str(i+1).zfill(6)
+                                     for i in similar_notes_indices]
+            print(f'\t{Fore.CYAN}Method {method.__name__}, arg {i}{Fore.RESET}')
+            print(
+                f'\t{Fore.MAGENTA}Found {len(similar_notes_indices)} similar notes in train set...{Fore.RESET}')
 
-    def classify(self):
-        classes = 10
-        print(f"{Fore.CYAN}Tokenizing...{Fore.RESET}")
-        tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', analyzer='word',
+    def tfidf(self, documents):
+        documents = [" ".join(doc) for doc in documents]
+        print(f"{Fore.CYAN}Tokenizing... {len(documents)}{Fore.RESET}")
+        print(documents[0])
+        tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', analyzer='word', lowercase=False,
                                 ngram_range=(1, 2), stop_words=self.stop_words)
-        features = tfidf.fit_transform(
-            np.random.choice(self.notes, size=(150, )))
-        print(f"{Fore.YELLOW}Reducing with Truncated SVD...{Fore.RESET}")
-        pca = TruncatedSVD(n_components=5)
-        features_t = pca.fit_transform(features)
-        print(f"{Fore.MAGENTA}Projecting with TSNE...{Fore.RESET}")
-        tsne = TSNE(n_components=2, perplexity=5)
-        Y = tsne.fit_transform(features_t)
-
-        color_list = [['#'+''.join([random.choice('0123456789ABCDEF')
-                                    for j in range(6)])] for i in range(classes)]
-        mb = MiniBatchKMeans(n_clusters=classes)
-        mb.fit_transform(Y)
-        labels = mb.labels_
-        feature_names = np.array(tfidf.get_feature_names())
-        for i, label in enumerate(np.unique(labels)):
-            plt.scatter(Y[labels == label, 0],
-                        Y[labels == label, 1], c=color_list[i], label=feature_names[label])
-        plt.title("Klasyfikacja PAP")
-        plt.legend()
-        plt.show()
+        features = tfidf.fit_transform(documents)
+        return features
 
 
 if __name__ == "__main__":
     n = Notes()
-    # n.load_notes()
-    # n.classify()
-    n.graph_representation(3)
+    n.classify_and_evaluate(5, 100)
